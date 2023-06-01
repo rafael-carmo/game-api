f6f7d3709470ba887b68a37bd0b3aee36fcb706d
rafael <rafaelljj@gmail.com>
2023-06-01 12:43:16 -0300

    :lipstick: feat: create feature register game

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+
+import { router } from './routes';
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(router);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
