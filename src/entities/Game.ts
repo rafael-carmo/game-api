ec488d92060360adcd8d07e8286e25717a6ef70d
rafael <rafaelljj@gmail.com>
2023-06-01 09:36:39 -0300

    :lipstick: feat: create entity

---
+++ src/entities/Game.ts
@@ -0,0 +1,22 @@
+import { randomUUID } from 'crypto';
+
+class Game {
+  id?: string;
+  name!: string;
+  designer!: string;
+  genre!: string;
+  platform!: string;
+  developer!: string;
+  releaseData!: string;
+  mode!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = randomUUID();
+    }
+  }
+}
+
+export { Game };
